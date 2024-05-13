c0ffe330f606718e556147fdd6a656455db18744
azurata <git@azurata.me>
2024-05-13 17:46:56 +0900

    feat: とりあえずエンドポイントだけ用意した

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/ping', (req, res) => {
+  res.send('pong')
 })
 
-app.get('/topics/')
+// スレ一覧
+app.get('/topics/', (req, res) => {
+  throw Error('No implementation yet');
+})
+
+// レス一覧
+app.get('/topic/:id/posts/', (req, res) => {
+  throw Error('No implementation yet');
+})
+
+// レスする
+app.post('/topic/:id/post/', (req, res) => {
+  throw Error('No implementation yet');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
