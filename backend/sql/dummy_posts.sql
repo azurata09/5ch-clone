2adc1b82975a3fb0fc31e53ce53e4406662eafe7
azurata <git@azurata.me>
2024-06-23 20:03:46 +0900

    refactor: ダミーデータのSQLファイルを記述

---
+++ backend/sql/dummy_posts.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts(topic_id, poster, content) VALUES(1, "ほげ", "ほげふが");
+INSERT INTO posts(topic_id, poster, content) VALUES(1, "ふが", "ふがぴよ");
+INSERT INTO posts(topic_id, poster, content) VALUES(1, "ぴよ", "ぴよほげ");
---
+++ backend/sql/dummy_topics.sql
@@ -0,0 +1,3 @@
+INSERT INTO topics(title) VALUES("ほげ");
+INSERT INTO topics(title) VALUES("ふが");
+INSERT INTO topics(title) VALUES("ぴよ");
